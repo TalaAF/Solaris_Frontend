f09185b013448489cd4e45d51da4c4c2ce85666e
SajaShawawra <202202357@bethlehem.edu>
2025-04-26 11:09:12 +0300

    Refactor Dashboard component to integrate multiple sections including Header, Sidebar, WelcomeSection, QuickAccessSection, ScheduleSection, ProgressSection, and DeadlinesSection; implement ChatBot functionality with context-aware responses and styling.

--- Solaris/src/components/Header.jsx
+++ Solaris/src/components/Header.jsx
@@ -22,27 +22,12 @@ const Header = () => {
       </div>
 
       <div className="header-icons">
-        <button className="icon-button search-button">
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <circle cx="11" cy="11" r="8"></circle>
-            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-          </svg>
-        </button>
-        
         <button className="icon-button notification-button">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
             <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
           </svg>
           <span className="notification-dot orange-dot"></span>
-        </button>
-        
-        <button className="icon-button mail-button">
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-            <polyline points="22,6 12,13 2,6"></polyline>
-          </svg>
-          <span className="notification-dot blue-dot"></span>
         </button>
         
         <button className="icon-button user-button">
